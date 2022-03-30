8a1a882544ff9bf90ae4924a68edb350fa34d2ab
nkinalis <nikolaskinalis@hotmail.com>
2022-03-30 12:08:13 -0400

    added logging middleware

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const ShareDB = require("sharedb");
 ShareDB.types.register(require("rich-text").type);
-const sharedb_server = new ShareDB();
-const sharedb_connection = server.connect();
+const sharedb_server = new ShareDB({
+  //use mongodb driver
+  db: require("sharedb-mongo")("mongodb://localhost:27017/sharedb"),
+});
+//use middleware for reply
+sharedb_server.use("reply", (context, next) => {
+  console.log(context.reply);
+});
+
+const sharedb_connection = sharedb_server.connect();
 
 const express = require("express");
 const api = express();
