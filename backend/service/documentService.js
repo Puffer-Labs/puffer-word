42c6cdea9e2e0c544a90f227c34db2a3ed26ea60
nkinalis <nikolaskinalis@hotmail.com>
2022-03-31 22:08:29 -0400

    implemented html return

--- backend/service/documentService.js
+++ backend/service/documentService.js
@@ -1,24 +1,38 @@
 const ShareDB = require("../config/sharedbConfig");
+const document = ShareDB.document;
 
 const getDocument = (id, res) => {
-  ShareDB.document.fetch(() => { //try to grab existing document
+  document.fetch(() => {
+    //try to grab existing document
     if (document.type === null) {
-      document.create([{ insert: "" }], "rich-text", () => {
-        res.set("Content-Type", "text/event-stream"); //send init message
-        res.write("data: " + JSON.stringify(document.data) + "\n\n");
-      });
+      document.create(
+        [
+          { insert: "Gandalf", attributes: { bold: true } },
+          { insert: " the " },
+          { insert: "Grey", attributes: { color: "#ccc" } },
+        ],
+        "rich-text",
+        () => {
+          res.set("Content-Type", "text/event-stream"); //send init message
+          res.write(
+            "data: " + JSON.stringify({ content: document.data.ops }) + "\n\n"
+          );
+        }
+      );
       return;
     }
     res.set("Content-Type", "text/event-stream"); //if document exists, send existing ops
-    res.write("data: " + JSON.stringify(document.data) + "\n\n");
+    res.write(
+      "data: " + JSON.stringify({ content: document.data.ops }) + "\n\n"
+    );
   });
-  document.on("op", (op, source) => { //have the client listen for changes
+  document.on("op", (op, source) => {
+    //have the client listen for changes
     res.write("data: " + JSON.stringify(op) + "\n\n");
   });
 };
 
 const getDocumentData = (id, res) => {
-  const document = ShareDB.sharedb_connection.get("documents", "default");
   document.fetch(() => {
     document.submitOp([{ retain: 1 }, { insert: "a" }], () => {
       res.json(document.data);
@@ -26,4 +40,13 @@ const getDocumentData = (id, res) => {
   });
 };
 
-module.exports = { getDocument, getDocumentData };
+//get document as html
+const getDocumentHTML = (id, res) => {
+  document.fetch(() => {
+    //try to grab existing document
+    const delta_contents = JSON.stringify(document.data);
+    res.render("index", { delta: delta_contents });
+  });
+};
+
+module.exports = { getDocument, getDocumentData, getDocumentHTML };
