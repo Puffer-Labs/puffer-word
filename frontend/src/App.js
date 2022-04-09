8d28f5a7b482ecdf45a06ddc9bfd7045bc62d607
Sherzod <shermanjones99@gmail.com>
2022-04-08 23:38:44 -0400

    fixed presence bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Quill from "quill";
 import QuillCursors from "quill-cursors";
+import cursors from "./cursors";
 import "quill/dist/quill.snow.css";
 
 //generate random client id
@@ -11,8 +12,6 @@ const getId = () => {
     Math.random().toString(36).substring(2, 15)
   );
 };
-
-const cursors = {};
 
 const App = () => {
   React.useEffect(() => {
@@ -38,9 +37,7 @@ const App = () => {
     let quill = new Quill("#editor", options);
 
     //init this client's cursor
-    const cursor = quill.getModule("cursors");
-    cursor.createCursor(id, id, "red");
-    cursors[id] = cursor;
+    cursors.init(quill);
 
     // Connect to the event source to listen for incoming operation changes
     const connection = new EventSource("http://localhost:8000/connect/" + id);
@@ -54,33 +51,15 @@ const App = () => {
        * 3. {data: {cursor: {connClosed: boolean, id: string, position: number}}} This is the cursor position of one of the clients that was updated.
        */
       const data = JSON.parse(event.data);
-      //if the change coming in is this client's cursor, ignore it
-      if (data.cursor && data.cursor.id !== id) {
-        const cursor = quill.getModule("cursors");
-        const cursorClient = cursors[data.cursor.id];
-        if (cursorClient) {
-          if (data.cursor.connClosed) {
-            //remove cursor object from table when cursor client disconnects
-            console.log("removing cursor");
-            cursor.removeCursor(cursorClient.id);
-            delete cursors[cursorClient.id];
-          } else {
-            cursor.moveCursor(
-              cursorClient.id,
-              data.cursor.range
-            );
-          }
-        } else {
-          console.log("creating cursor");
-          cursors[data.cursor.id] = cursor.createCursor(data.cursor.id, data.cursor.id, "blue");
-        }
+      if (cursors.isRemoteCursorEvent(data, id)) {
+        cursors.processCursorEvent(data.cursor);
       } else if (data.content) {
         quill.setContents(data.content);
       } else {
         data.map((op) => quill.updateContents(op));
       }
     };
-
+    
     // client -> server
     quill.on("text-change", (delta, oldDelta, source) => {
       /**
@@ -100,7 +79,7 @@ const App = () => {
       }
     });
 
-    quill.on("selection-change", selectionChangeHandler(cursors[id], id));
+    quill.on("selection-change", selectionChangeHandler(id));
 
     // When the component is unmounted, we need to close the connection
     return () => {
@@ -108,7 +87,7 @@ const App = () => {
     };
   }, []);
 
-  function selectionChangeHandler(cursor, id) {
+  function selectionChangeHandler(id) {
     return function (range, oldRange, source) {
       if (range && source === "user") {
         console.log(range);
