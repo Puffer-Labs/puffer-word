8895a1d7e157851c83a3354bd222c876965cab8d
Sherzod <shermanjones99@gmail.com>
2022-04-11 10:42:24 -0400

    added media upload functionality

---
+++ backend/controller/mediaController.js
@@ -0,0 +1,39 @@
+/**
+ * Routes for media endpoints
+ */
+
+const express = require("express");
+const router = express.Router();
+const mediaService = require("../service/mediaService");
+
+/**
+ * Upload image to the /uploads directory.
+ * @returns {string} The media ID of the uploaded image.
+ */
+router.post(
+  "/upload",
+  mediaService.upload.single("image"),
+  (req, res) => {
+    // Save uploaded file and its mime type and return its ID.
+    res.json({
+      mediaid: req.file.filename.split(".").shift(),
+    });
+  },
+  (err, req, res, next) => {
+    res.status(400).send({ error: err.message });
+  }
+);
+
+/**
+ * Fetch the image with the given media ID.
+ * @returns {string} The image data.
+ */
+router.get("/access/:mediaid", (req, res) => {
+  try {
+    mediaService.getFile(res, req.params.mediaid);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,12 @@ api.use(express.urlencoded({ extended: true }));
 api.use(cors());
 // parser(api);
 
-const documentController = require('./controller/documentController');
-api.use('/', documentController);
-api.get('/', (req, res) => {
-	res.send('Hello World!');
+const documentController = require("./controller/documentController");
+const mediaController = require("./controller/mediaController");
+api.use("/media", mediaController);
+api.use("/", documentController);
+api.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 api.listen(port, () => {
