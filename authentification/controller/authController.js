98847b6e579fa4a73c91203b87932168c6b15bac
Mark <msmoawad99@gmail.com>
2022-04-12 16:47:11 -0400

    bouta rip passport out

--- authentification/controller/authController.js
+++ authentification/controller/authController.js
@@ -1,50 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const passport = require('../config/passportConfig');
-const authService = require('../service/authService');
-const isLoggedIn = require('../service/middleware');
+const passport = require("../config/passportConfig");
+const authService = require("../service/authService");
+const isLoggedIn = require("../service/middleware");
 
-router.get('/users', (req, res) => {
-	authService.getAllUsers(res);
+router.get("/users", (req, res) => {
+  authService.getAllUsers(res);
 });
 
-router.post('/users/create', (req, res) => {
-	authService.createUser(req, res);
+router.post("/users/create", (req, res) => {
+  authService.createUser(req, res);
 });
 
-router.post('/login', (req, res) => {
-	req.login(req.body, (err) => {
-		if (err) {
-			res.status(500).send({ error: err.message });
-		} else {
-			//create cookie that contains user's id and expires in 6 hours
-			res.cookie('user', req.user.username, {
-				path: '/',
-				expires: new Date(Date.now() + 6 * 60 * 60 * 1000)
-			});
-			res.status(200).send('Logged in');
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureMessage: "Invalid username or password",
+    failWithError: true,
+  }),
+  (req, res) => {
+    res.cookie("user", req.user.username, {
+      path: "/",
+      maxAge: 10 * 1000,
+    });
+    res.status(200).send({ name: req.user.username });
+  }
+);
 
-		}
-	});
+router.get("/me", isLoggedIn, (req, res) => {
+  res.status(200).send(req.user);
 });
 
-router.get('/me', isLoggedIn, (req, res) => {
-
-	res.status(200).send(req.user);
-});
-
-router.get('/logout', function(req,res){
-	req.logOut();
-	res.status(200).clearCookie('connect.sid', {
-	  path: '/',
-	  secure: false,
-	  httpOnly: false,
-	  domain: 'http://localhost:3000',
-	  sameSite: true,
-	});
-	req.session.destroy(function (err) {
-	  res.redirect('/');
-	});
+router.get("/logout", function (req, res) {
+  req.logOut();
+  res.status(200).clearCookie("connect.sid", {
+    path: "/",
+    secure: false,
+    httpOnly: false,
+    domain: "http://localhost:3000",
+    sameSite: true,
   });
+  req.session.destroy(function (err) {
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
--- authentification/index.js
+++ authentification/index.js
@@ -1,43 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const port = 8080;
-const passport = require('./config/passportConfig');
-const authController = require('./controller/authController');
-const cors = require('cors')
-require('./config/mongoConfig');
-
+const passport = require("./config/passportConfig");
+const authController = require("./controller/authController");
+const cors = require("cors");
+require("./config/mongoConfig");
 
 app.use(express.json());
-app.use(cors({
-	origin: 'http://localhost:3000',
-	credentials: true
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
-	session({
-		secret: 'fart',
-		saveUninitialized: false,
-		cookie: {
-			maxAge: 6 * 60 * 60 * 1000,
-		},
-		secure: true,
-		resave: true,
-		rolling: true //updates the cookie expiration time
-	})
+  session({
+    secret: "fart",
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 6 * 60 * 60 * 1000,
+    },
+    secure: false,
+    resave: true,
+    rolling: true, //updates the cookie expiration time
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/', authController);
+app.use("/", authController);
 
-app.get('/', (req, res) => {
-	res.send({ msg: 'Hello world' });
+app.get("/", (req, res) => {
+  res.send({ msg: "Hello world" });
 });
 
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
