61d0393ea85e328cbf9a4d2a8ac2a575d1a404fd
Mark Moawad <msmoawad99@gmail.com>
2022-04-13 00:05:33 -0400

    got passport working, need to test out logout

--- authentification/controller/authController.js
+++ authentification/controller/authController.js
@@ -1,48 +1,48 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const passport = require("../config/passportConfig");
-const authService = require("../service/authService");
-const isLoggedIn = require("../service/middleware");
+const passport = require('../config/passportConfig');
+const authService = require('../service/authService');
+const middleware = require('../service/middleware');
 
-router.get("/users", (req, res) => {
-  authService.getAllUsers(res);
+router.get('/users', (req, res) => {
+	authService.getAllUsers(res);
 });
 
-router.post("/users/create", (req, res) => {
-  authService.createUser(req, res);
+router.post('/users/create', (req, res) => {
+	authService.createUser(req, res);
 });
 
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    failureMessage: "Invalid username or password",
-    failWithError: true,
-  }),
-  (req, res) => {
-    res.cookie("user", req.user.username, {
-      path: "/",
-      maxAge: 10 * 1000,
-    });
-    res.status(200).send({ name: req.user.username });
-  }
-);
+// router.post('/login', middleware.login, (req, res) => {
+// 	res.cookie('user', req.user.username, {
+// 		path: '/',
+// 		maxAge: 10 * 1000,
+// 		sameSite: true,
+// 		secure: false
+// 	});
+// 	res.status(200).send({ name: req.user.username, session: req.session });
+// });
 
-router.get("/me", isLoggedIn, (req, res) => {
-  res.status(200).send(req.user);
+router.post('/login', middleware.authorize, (req, res) => {
+	res.cookie('user', req.user.username, {
+		path: '/',
+		maxAge: 10 * 1000,
+		sameSite: true,
+		secure: false
+	});
+	res.status(200).send({ name: req.user.username });
 });
 
-router.get("/logout", function (req, res) {
-  req.logOut();
-  res.status(200).clearCookie("connect.sid", {
-    path: "/",
-    secure: false,
-    httpOnly: false,
-    domain: "http://localhost:3000",
-    sameSite: true,
-  });
-  req.session.destroy(function (err) {
-    res.redirect("/");
-  });
+router.get('/me', middleware.isLoggedIn, (req, res) => {
+	console.log(req.user);
+	res.status(200).send(req.user);
+});
+
+router.get('/logout', middleware.isLoggedIn, middleware.logout, (req, res) => {
+	req.session.destroy(function(err) {
+		res.clearCookie('connect.sid');
+		res.clearCookie('user');
+		res.status(200).send({ msg: 'logged out' });
+	});
 });
 
 module.exports = router;
--- authentification/service/middleware.js
+++ authentification/service/middleware.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const User = require('../schema/user');
+const bcrypt = require('bcrypt');
+const passport = require('../config/passportConfig');
 
 const isLoggedIn = (req, res, next) => {
-	console.log(req.session)
 	if (req.session.passport) {
 		next();
 	} else {
@@ -9,4 +10,45 @@ const isLoggedIn = (req, res, next) => {
 	}
 };
 
-module.exports = isLoggedIn;
+// const login = async (req, res, next) => {
+// 	const { username, password } = req.body;
+// 	let user = await User.findOne({ username: username });
+// 	if (!user) {
+// 		res.status(401).send('User not found');
+// 	}
+// 	bcrypt.compare(password, user.password, (err, result) => {
+// 		if (err) {
+// 			res.status(500).send({ error: err.message, msg: 'error comparing passwords' });
+// 		}
+// 		if (result) {
+// 			req.session.login = { user: user.username };
+// 			req.user = user;
+// 			next();
+// 		}
+// 	});
+// };
+
+const authorize = (req, res, next) => {
+	passport.authenticate('local', {}, (err, user) => {
+		console.log(err);
+		if (err) {
+			res.status(500).send({ error: err.message });
+		}
+		if (!user) {
+			res.status(401).send('User not found');
+		}
+		if (user) {
+			req.session.passport = { user: user.username };
+			req.user = user;
+			next();
+		}
+	})(req, res, next);
+};
+
+const logout = (req, res, next) => {
+	req.session.passport = null;
+	req.user = null;
+	next();
+};
+
+module.exports = { isLoggedIn, logout, authorize };
