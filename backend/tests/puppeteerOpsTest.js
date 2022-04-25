81dd2447bde00b3e7c51863bcd678e5021be37f4
alex <alexperez52@gmail.com>
2022-04-25 03:23:31 -0400

    Documentation

--- backend/tests/puppeteerOpsTest.js
+++ backend/tests/puppeteerOpsTest.js
@@ -1,82 +1,54 @@
 const puppeteer = require("puppeteer");
-const { Cluster } = require('puppeteer-cluster');
-
-
+const { Cluster } = require("puppeteer-cluster");
 
 (async () => {
+    // Runs 9 clients in parallel
     const cluster = await Cluster.launch({
-      concurrency: Cluster.CONCURRENCY_CONTEXT,
-      maxConcurrency: 9,
-      monitor: true,
-      timeout: 500000
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 9,  // Change this number for more workers
+        monitor: true,
+        timeout: 500000,
     });
-  
-    await cluster.task(async ({ page, data: email}) => {
+
+    // Email is sent as data to be used for login -
+    // Requires accounts to already be registered in the format:
+    // test<num>@tst.com with password 'test'
+    await cluster.task(async ({ page, data: email }) => {
         const browser = await puppeteer.launch({ headless: true });
-        // const page = await browser.newPage();
-    
         await page.goto("http://localhost:3000");
         await page.type("#login-username", email);
         await page.type("#login-password", "test");
         await page.click("#login-btn"),
             await page.waitForNavigation({ waitUntil: "networkidle0" });
-           const tag = await page.$x("//a[contains(., doc1)]");
-           await Promise.all([
+        const tag = await page.$x("//a[contains(., doc1)]");
+        await Promise.all([
             tag[0].click(),
-            page.waitForNavigation({ waitUntil: 'networkidle0' }),
-          ]);
-        //   await Promise.all([
-        //     page.type('input[name=editor]', 'Hello World!', { delay: 100 }),
-        //     page.waitForNavigation({ waitUntil: 'networkidle0' }),
-        //   ]);
+            page.waitForNavigation({ waitUntil: "networkidle0" }),
+        ]);
+        // Pressing tab until clients can type into quill since we cannot identify
+        // the quill element by any css attribute
         for (let i = 0; i < 9; i++) {
             await page.keyboard.press("Tab");
         }
 
+        // Types Hello World! into quill text editor all at the same time
         await page.type("#editor", "Hello World!", { delay: 1000 });
-            // await page.screenshot({ path: "./example.png" });
-            await page.waitForNavigation({ waitUntil: "networkidle0" });
-            browser.close();
+        await page.waitForNavigation({ waitUntil: "networkidle0" });
+        browser.close();
     });
-  
-    cluster.queue('test1@test.com');
-     cluster.queue('test2@test.com');
-    cluster.queue('test3@test.com');
-    cluster.queue('test4@test.com');
-    cluster.queue('test5@test.com');
-    cluster.queue('test6@test.com');
-    cluster.queue('test7@test.com');
-    cluster.queue('test8@test.com');
-    cluster.queue('test9@test.com');
 
-    // many more pages
-  
+    /// Queues each worker client account
+    cluster.queue("test1@test.com");
+    cluster.queue("test2@test.com");
+    cluster.queue("test3@test.com");
+    cluster.queue("test4@test.com");
+    cluster.queue("test5@test.com");
+    cluster.queue("test6@test.com");
+    cluster.queue("test7@test.com");
+    cluster.queue("test8@test.com");
+    cluster.queue("test9@test.com");
+    // Add more workers here
+
     await cluster.idle();
     await cluster.close();
-  })();
-// (async () => {
-//     const browser = await puppeteer.launch({ headless: false });
-//     const page = await browser.newPage();
-
-//     await page.goto("http://localhost:3000");
-//     await page.type("#login-username", "test1@test.com");
-//     await page.type("#login-password", "test");
-//     await page.click("#login-btn"),
-//         await page.waitForNavigation({ waitUntil: "networkidle0" });
-//        const tag = await page.$x("//a[contains(., doc1)]");
-//        await Promise.all([
-//         tag[0].click(),
-//         page.waitForNavigation({ waitUntil: 'networkidle0' }),
-//       ]);
-//     //   await Promise.all([
-//     //     page.type('input[name=editor]', 'Hello World!', { delay: 100 }),
-//     //     page.waitForNavigation({ waitUntil: 'networkidle0' }),
-//     //   ]);
-//     for (let i = 0; i < 9; i++) {
-//         await page.keyboard.press("Tab");
-//     }
-//     await page.type("#editor", "Hello World!", { delay: 100 });
-//         await page.screenshot({ path: "./example.png" });
-
-//     await browser.close();
-// })();
+})();
